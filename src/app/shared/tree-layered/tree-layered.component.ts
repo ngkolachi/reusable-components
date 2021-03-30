4a0b9d652f0100af5219fd98bb273096aef52b5e
Muhammad Awais <awaisshaikh94@gmail.com>
2021-03-31 01:17:03 +0500

    app init

---
+++ src/app/shared/tree-layered/tree-layered.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tree-layered',
+  templateUrl: './tree-layered.component.html',
+  styleUrls: ['./tree-layered.component.scss']
+})
+export class TreeLayeredComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
